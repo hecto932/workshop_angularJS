f1bed27e2ba3733f1ef93c4755ae52dcb9f83727
Hector Flores <hecto932@gmail.com>
2016-04-28 13:19:00 -0430

    Creating Login App

--- controller.js
+++ controller.js
@@ -3,12 +3,25 @@ var app = angular.module('mainApp', ['ngRoute']);
 app.config(function($routeProvider){
 	$routeProvider
 		.when('/', {
-			templateUrl: 'page.html'
-		})
-		.when('/login', {
 			templateUrl: 'login.html'
+		})
+		.when('/dashboard', {
+			templateUrl: 'dashboard.html'
 		})
 		.otherwise({
 			redirectTo: '/'
-		})
+		});
 });
+
+app.controller('loginCtrl', function($scope, $location){
+	$scope.submit = function (){
+		var username = $scope.username;
+		var password = $scope.password;
+		if($scope.username == 'admin' && $scope.password == 'admin'){
+			$location.path('/dashboard');
+		}
+		else{
+			alert("Wrong stuff.");
+		}
+	}
+})
