db37bac7d3fe99f7564817790bebb2328537de63
Hector Flores <hecto932@gmail.com>
2016-05-08 14:43:49 -0400

    Services in AngularJS

---
+++ script23.js
@@ -0,0 +1,18 @@
+var application = angular.module('mainApp', []);
+
+//application.controller();
+
+application.service('random', function(){
+
+	var num = Math.floor(Math.random()*10);
+	this.generate = function(){
+		return num;
+	}
+
+});
+
+application.controller('app', function($scope, random){
+	$scope.generateRandom = function(){
+		$scope.randomNumber = random.generate();
+	}
+})
